d15eeb0214dcad9ed9a71eadd0ec1abe510142a7
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-11-01 09:21:48 +0000

    commit tree c38d54187045f8a437d81b29eea8b7b278d275fa

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  strArr = str.split("")
+  rvArr = []
+  strArr.forEach(char => rvArr.unshift(char));
+  rvStr= rvArr.join("")
+  return rvStr
 }
 
 if (require.main === module) {
@@ -11,9 +16,37 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("Expecting: 'sugnA'")
+  console.log("=>", reverseString("Angus"))
+
+  console.log("Expecting: '54321'")
+  console.log("=>", reverseString('12345'))
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+/* 
+
+THE PROBLEM
+
+- Write a function/method that receives one argument, a string
+
+- It returns a string, with the original string's letters reversed
+
+PSEUDOCODE
+
+Step one, split the string into array of characters with .split("")
+
+iterate over each character of string and add each to beginning of array
+
+join arrray with .join("")
+
+return final string
+
+
+*/
